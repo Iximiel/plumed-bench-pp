b12a6592583cf5565530dbe99ef46a8adb478665
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-07-16 15:07:30 +0200

    clearer types, added doculemtation

--- src/plumed_bench_pp/tabulate.py
+++ src/plumed_bench_pp/tabulate.py
@@ -10,16 +10,27 @@ from plumed_bench_pp.constants import (
 
 def extract_row(
     data: dict,
-    cols: list,
-) -> "dict[str, list]":
-    df: dict[str, list] = {}
+    rows: list,
+) -> "dict[str, dict[str,list]]":
+    """
+    Extracts the specified rows from the given data dictionary.
+    Works with the results of plumed_bench_pp.parser.parse_benchmark_output
+
+    Args:
+        data (dict): The input dictionary containing data.
+        rows (list): The list of rows to extract.
+
+    Returns:
+        dict[str, dict[str,list]]: A dictionary of the simulations.
+    """
+    df = {}
     for key in data:
         if key == "BENCHSETTINGS":
             continue
         loc = f'{key}+{data[key]["input"]}'
-        df[loc] = {}
-        for col in cols:
-            t = [data[key][col][timing_col] for timing_col in TIMINGCOLS]
-            df[loc][col] = t
+        tmp = {}
+        for row in rows:
+            tmp[row] = [data[key][row][timing_col] for timing_col in TIMINGCOLS]
+        df[loc] = tmp
 
     return df
