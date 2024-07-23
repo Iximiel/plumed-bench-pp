7c72774dbd16c7333188f3bf48627fefd83c8d41
Daniele Rapetti <iximiel@gmail.com>
2024-07-23 10:29:57 +0200

    this may help documentation?

--- src/plumed_bench_pp/__init__.py
+++ src/plumed_bench_pp/__init__.py
@@ -2,3 +2,12 @@
 #
 # SPDX-License-Identifier: MIT
 
+from plumed_bench_pp import constants, parser, plot, tabulate, utils
+
+__all__ = [
+    "constants",
+    "plot",
+    "parser",
+    "tabulate",
+    "utils",
+]
