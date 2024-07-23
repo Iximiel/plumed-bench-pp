4387756e4455e611e662c4a2233580f3fd7e6bf7
Daniele Rapetti <iximiel@gmail.com>
2024-07-23 16:59:39 +0200

    changed some settings in sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,14 @@ release = "0.0.3a0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "myst_parser"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "myst_parser",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -27,5 +34,22 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "furo"
 html_static_path = ["_static"]
+
+autodoc_default_options = {
+    "special-members": "__init__",
+    "inherited-members": True,
+    "show-inheritance": True,
+    "ignore-module-all": True,
+}
+
+intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+    "numpy": ("http://docs.scipy.org/doc/numpy", None),
+    # this creates crashes in the docs
+    # "pandas": ('https://pandas.pydata.org/docs/', None)
+}
+intersphinx_disabled_domains = ["std"]
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -3,14 +3,13 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-plumed-bench-pp documentation
-=============================
-
+.. include:: ../../README.md
+    :parser: myst_parser.sphinx_
 
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
    
-   readme_link
+
    documentation
