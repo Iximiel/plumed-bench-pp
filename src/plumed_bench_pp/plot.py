d9b40f909a407aac4e4241b874d5b1ca04ca1d88
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-07-30 11:14:26 +0200

    schanged how x axis behaviour for plotting the histogram

--- src/plumed_bench_pp/plot.py
+++ src/plumed_bench_pp/plot.py
@@ -10,7 +10,6 @@ if TYPE_CHECKING:
     from typing import Any
 
     from matplotlib.container import BarContainer
-    from numpy import ndarray
     from pandas import DataFrame
 
 from matplotlib.pyplot import Axes
@@ -23,16 +22,18 @@ def plot_histo(
     barwidth: float = 0.8,
     *,
     normalize_to_cycles: "bool|str" = False,
-    x: "ndarray|None" = None,
     colors: "list|None" = None,
     relative_to: "dict[str, DataFrame]| Any" = None,
     relative_to_row: "str|None" = None,
     titles: "list[str]|None" = None,
+    equidistant_bars: bool = True,
 ) -> "list[BarContainer]":
     """
     Plot a histogram based on the provided data for a specified row.
 
     It can be set up to normalize the data to cycles (normalize_to_cycles) and/or relative to another row.
+    `normalize_to_cycles` can be either a boolean flag or the name of the row to use as base for the cycles:
+    This is useful when using the TOTALTIME row, that is reported as a single cycle.
 
     It can be set up to use a custom x-axis or colors for the bars.
 
@@ -43,8 +44,7 @@ def plot_histo(
         data (list[dict]|dict[str,DataFrame]): The data to be plotted.
         row (str): The row in the data to be plotted.
         barwidth (float, optional): The width of the bars in the histogram. Defaults to 0.8.
-        normalize_to_cycles (bool|str, optional): Flag to normalize data to cycles. Defaults to False.
-        x (np.array|None, optional): The x values for plotting. Defaults to None.
+        normalize_to_cycles (bool|str, optional): Flag to normalize data to cycles, if set to a row name the cycles of that row will be used. Defaults to False.
         colors (list|None, optional): The colors for the bars in the histogram. Defaults to None.
         relative_to (dict[str, DataFrame]| Any, optional): Data for relative comparison, you can pass a dict contianing the collection of data or the address or index of the desired base in the passed data. Defaults to None.
         relative_to_row (str|None, optional): The row for relative comparison. Defaults to None.
@@ -74,10 +74,12 @@ def plot_histo(
             divideby = divideby / relative_to[row_cycles].Cycles.values
 
     ncols = len(data)
-    if x is None:
-        x = data[0][row].natoms.values
+    # num_bars=np.max([len(d[row]) for d in data])
+
+    x = np.arange(len(data[0][row])) if equidistant_bars else data[0][row].natoms.values
+    xnames = data[0][row].natoms.values
     width = np.min(np.diff(np.sort(x))) * (barwidth / ncols)
-    ax.set_xticks(x + width * 0.5 * (ncols - 1), x)
+    ax.set_xticks(x + width * 0.5 * (ncols - 1), xnames)
     bars = []
     for multiplier, d in enumerate(data):
         offset = width * multiplier
@@ -109,24 +111,24 @@ def plot_histo_relative(
     relative_to: "dict[str, DataFrame]| Any",
     relative_to_row: "str|None" = None,
     barwidth: float = 0.8,
-    *,
-    normalize_to_cycles: "bool|str" = False,
-    x: "ndarray|None" = None,
-    colors: "list|None" = None,
+    **kwargs,
 ) -> "list[BarContainer]":
     """
     Plots a histogram of the given data relative to the `relative_to` data.
 
     Args:
         ax (matplotlib.axes.Axes): The axes on which to plot the histogram.
-        data (list[dict] | dict[str, pandas.DataFrame]): The data to plot.
+        data (list[dict] | dict[str, pandas.DataFrame]): The data to plot. It can be a list of dictionaries where each
+            dictionary contains a DataFrame indexed by the name of a kernel. Alternatively, it can be a single dictionary
+            where the keys are the names of the kernels and the values are the corresponding DataFrames.
         row (str): The row to plot.
         relative_to (dict[str, pandas.DataFrame] | Any): The data to which the values of the current data are relative to.
-        relative_to_row (str | None): The row in the `relative_to` data to use for the reference values.
+            It can be a dictionary where the keys are the names of the kernels and the values are the corresponding DataFrames.
+            Alternatively, it can be a string representing the name of a kernel in the `data` dictionary.
+        relative_to_row (str | None): The row in the `relative_to` data to use for the reference values. If None, the row
+            specified by `row` is used.
         barwidth (float): The width of the bars in the histogram.
-        normalize_to_cycles (bool | str): If True, the values of the current data are normalized to the number of cycles.
-        x (numpy.ndarray | None): The x values to use for the plot. If None, the x values are determined by the `natoms` values in the current data.
-        colors (list | None): The colors to use for the bars in the histogram.
+        **kwargs: Additional keyword arguments passed to `plot_histo`.
 
     Returns:
         list[matplotlib.container.BarContainer]: The bars plotted in the histogram.
@@ -139,9 +141,7 @@ def plot_histo_relative(
         data,
         row,
         barwidth=barwidth,
-        normalize_to_cycles=normalize_to_cycles,
-        x=x,
-        colors=colors,
         relative_to=relative_to,
         relative_to_row=relative_to_row,
+        **kwargs,
     )
