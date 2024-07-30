5176cd04833cb39b3818f5857bd8e14c02dee433
Daniele Rapetti <5535617+Iximiel@users.noreply.github.com>
2024-07-30 14:34:11 +0200

    Used the number of atoms as indexes in tabulate

--- src/plumed_bench_pp/tabulate.py
+++ src/plumed_bench_pp/tabulate.py
@@ -87,5 +87,5 @@ def convert_to_table(
             tmp[row].append([natoms, *tt[key][row]])
 
     for row in rows_to_extract:
-        data[row] = DataFrame(tmp[row], columns=["natoms", *TIMINGCOLS]).sort_values(by="natoms")
+        data[row] = DataFrame(tmp[row], columns=["natoms", *TIMINGCOLS]).set_index("natoms").sort_index()
     return data
